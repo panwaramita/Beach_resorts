5bef840decf1b18c36d773b0f9c730acaae1048d
Amita <panwar.amita@gmail.com>
2021-07-15 21:33:04 -0400

    added home page

---
+++ src/pages/home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import image from "../images/bres.png";
+import style from "../style/home.css";
+const home=()=>{
+    return(
+<img src={image} alt="resort"></img>
+)
+
+}
+
+export default home;
